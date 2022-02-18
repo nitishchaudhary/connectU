c7fb04ec8d63e671864f5867ed5f0e029acc7a01
nitishchaudhary <006nschaudhary006@gmail.com>
2022-02-18 12:10:29 +0530

    Basic styling and functionality done

--- Main/models.py
+++ Main/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     pic  =  models.ImageField(default = 'default.jpg',upload_to = 'profile_images')
     about = models.TextField()
+    
     def __str__(self):
         return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Message(models.Model):
     sender_id = models.ForeignKey(User,related_name="sender",on_delete=models.CASCADE)
@@ -16,4 +28,3 @@ class Message(models.Model):
     message = models.CharField(max_length=255)
     mesage_date = models.DateTimeField(default=timezone.now)
     image = models.ImageField(upload_to = 'message_media',blank=True)
-
